98d047141a44a9accb01dcc73e3ebb93a2e51b8e
FairyPenguin <autorun81@gmail.com>
2022-12-13 01:32:03 +0200

    add the build version

---
+++ dist/index.97569e15.js
@@ -0,0 +1,2 @@
+const buttons=document.querySelectorAll(".color-btn");buttons.forEach((t=>t.addEventListener("click",(()=>{document.documentElement.style.setProperty("--hue",t.dataset.color)}))));
+//# sourceMappingURL=index.97569e15.js.map
